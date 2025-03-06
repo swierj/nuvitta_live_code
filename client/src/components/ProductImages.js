7bb7513e044dda343e4af1c9d8b1ec6c00e419b0
swierj <jacobswierz07@gmail.com>
2025-03-06 00:21:33 -0800

    back at it!!!

--- client/src/components/ProductImages.js
+++ client/src/components/ProductImages.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 //import ReactImageMagnify from 'react-image-magnify'
 // disabled react-image-magnify until I can get it to work. won't be showing main product image :(
+// https://www.npmjs.com/package/@datobs/react-image-magnifiers
 
 export default function ProductImages({ imgMain, name, imgAlt1, imgAlt2 }) {
   const images = [imgMain, imgAlt1, imgAlt2]
@@ -9,50 +10,54 @@ export default function ProductImages({ imgMain, name, imgAlt1, imgAlt2 }) {
   // eslint-disable-next-line
   const [main, setMain] = useState(images[0])
   return (
+    // <ImagesContainer>
+    //   <div className='img-wrapper'>
+    //     { <ReactImageMagnify
+    //       {...{
+    //         smallImage: {
+    //           alt: 'Wristwatch by Ted Baker London',
+    //           isFluidWidth: true,
+    //           src: main,
+    //         },
+    //         largeImage: {
+    //           src: main,
+    //           width: 1200,
+    //           height: 1600,
+    //         },
+    //         hoverDelayInMs: 150,
+    //         enlargedImagePosition: 'over',
+    //         isHintEnabled: true,
+    //         shouldHideHintAfterFirstActivation: false,
+    //       }}
+    //     /> }
+    //   </div>
+    //   {/* <div className='img-gallery'>
+    //     {images.map((image, index) => {
+    //       return (
+    //         <img
+    //           src={image}
+    //           alt=''
+    //           key={index}
+    //           className={`${image === main ? 'active' : null}`}
+    //           onClick={() => setMain(images[index])}
+    //         />
+    //       )
+    //     })}
+    //   </div> */}
+    // </ImagesContainer>
     <ImagesContainer>
       <div className='img-wrapper'>
-        {/* <ReactImageMagnify
-          {...{
-            smallImage: {
-              alt: 'Wristwatch by Ted Baker London',
-              isFluidWidth: true,
-              src: main,
-            },
-            largeImage: {
-              src: main,
-              width: 1200,
-              height: 1600,
-            },
-            hoverDelayInMs: 150,
-            enlargedImagePosition: 'over',
-            isHintEnabled: true,
-            shouldHideHintAfterFirstActivation: false,
-          }}
-        /> */}
+        <img src={main} alt={name} />
       </div>
-      {/* <div className='img-gallery'>
-        {images.map((image, index) => {
-          return (
-            <img
-              src={image}
-              alt=''
-              key={index}
-              className={`${image === main ? 'active' : null}`}
-              onClick={() => setMain(images[index])}
-            />
-          )
-        })}
-      </div> */}
     </ImagesContainer>
   )
 }
 
 const ImagesContainer = styled.section`
   img {
+    max-width: 100%;
   }
   img.mainImg:hover {
-  }
-  .img-wrapper {
   }
   .img-gallery {
     width: 30rem;
